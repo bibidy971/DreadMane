b524fcb4a9012fe6ea7e042cdcdb4d2b866b420a
bibidy971 <34426705+bibidy971@users.noreply.github.com>
2020-03-29 03:49:41 +0200

    Hash Map allUsers

--- app/src/main/java/com/example/dreadmane/viewModel/MyUserViewModel.kt
+++ app/src/main/java/com/example/dreadmane/viewModel/MyUserViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.dreadmane.data.User
+import com.example.dreadmane.fragments.ChapterFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -14,28 +15,28 @@ class MyUserViewModel : ViewModel() {
         const val TAG = "ViewModelUser"
     }
 
+    private var authUser = FirebaseAuth.getInstance().currentUser!!
+    private val database: DatabaseReference = FirebaseDatabase.getInstance().reference
+    private var mAllUsers: MutableLiveData<HashMap<String,User>> = MutableLiveData<HashMap<String,User>>()
+
     private val users: MutableLiveData<User> by lazy {
         MutableLiveData<User>().also {
             loadUsers()
         }
     }
 
-    private val mUserList: MutableLiveData<List<User>> by lazy {
-        MutableLiveData<List<User>>().also {
-            userList()
-        }
-
+    init {
+        loadAllUsers()
     }
 
-    private var authUser = FirebaseAuth.getInstance().currentUser!!
-    private val database: DatabaseReference = FirebaseDatabase.getInstance().reference
+
 
     fun getUsers(): LiveData<User> {
         return users
     }
 
-    private fun userList(){
-
+    fun userList(): MutableLiveData<HashMap<String, User>> {
+        return mAllUsers
     }
 
 
@@ -45,8 +46,10 @@ class MyUserViewModel : ViewModel() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
-                val post = dataSnapshot.getValue(User::class.java)
-                users.value = post
+                if (dataSnapshot.exists()) {
+                    val post = dataSnapshot.getValue(User::class.java)
+                    users.value = post
+                }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -58,5 +61,60 @@ class MyUserViewModel : ViewModel() {
         database.child("users").child(authUser.uid).addValueEventListener(postListener)
 
 
+    }
+
+    private fun loadAllUsers(){
+
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                // A new comment has been added, add it to the displayed list
+                val comment = dataSnapshot.getValue(User::class.java)
+                if (comment != null) {
+                    mAllUsers.value?.put(dataSnapshot.key!!,comment)
+                }
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(ChapterFragment.TAG, "onChildChanged: ${dataSnapshot.key}")
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+                val newComment = dataSnapshot.getValue(User::class.java)
+                val commentKey = dataSnapshot.key
+
+                if (newComment != null && commentKey != null && mAllUsers.value?.containsKey(commentKey)!!) {
+                    mAllUsers.value?.set(commentKey,newComment)
+                }
+
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(ChapterFragment.TAG, "onChildRemoved:" + dataSnapshot.key!!)
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                val commentKey = dataSnapshot.key
+
+                mAllUsers.value?.remove(commentKey)
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(ChapterFragment.TAG, "onChildMoved:" + dataSnapshot.key!!)
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+                val movedComment = dataSnapshot.getValue(User::class.java)
+                val commentKey = dataSnapshot.key
+
+                if (movedComment != null && commentKey != null && mAllUsers.value?.containsKey(commentKey)!!) {
+                    mAllUsers.value?.set(commentKey,movedComment)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                //TAG.w(TAG, "postComments:onCancelled", databaseError.toException())
+            }
+        }
+        database.child("users").addChildEventListener(childEventListener)
     }
 }
